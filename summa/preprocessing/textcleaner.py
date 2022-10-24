2cde02048e69600b75a9431f0540d8dc6773f511
virusser1109 <virusser1109@gmail.com>
2022-10-24 14:47:51 +0700

    add vncorenlp

--- summa/preprocessing/textcleaner.py
+++ summa/preprocessing/textcleaner.py
@@ -171,7 +171,7 @@ def clean_text_by_sentences(text, language="english", additional_stopwords=None)
     else:
         import py_vncorenlp
     
-        rdrsegmenter = py_vncorenlp.VnCoreNLP(annotators=["wseg"], save_dir="./vncorenlp")
+        rdrsegmenter = py_vncorenlp.VnCoreNLP(annotators=["wseg"], save_dir="vncorenlp")
 
         original_sentences = rdrsegmenter.word_segment(text)
         non_stopword_tokens = lambda tokens: remove_stopwords(tokens)
